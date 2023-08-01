dc89ead1688983c603baa93eb8760afdb5220d6f
gh-56 <dinggul56@gmail.com>
2023-08-01 11:24:46 +0900

    buy

--- src/User.java
+++ src/User.java
@@ -6,6 +6,14 @@ public class User {
     int espressoMoney = 4000;
     int latteMoney = 7000;
     int cappuccinoMoney = 6000;
+    int espressoWater = 250;
+    int latteWater = 350;
+    int cappuccinoWater = 200;
+    int espressoCoffeeBeans = 16;
+    int latteCoffeeBeans = 20;
+    int cappuccinoCoffeeBeans = 12;
+    int latteMilk = 75;
+    int cappuccinoMilk = 100;
     void buy(){
         System.out.println("어떤 커피를 구매하시겠습니까?");
         System.out.println("1. 에스프레소 (가격 : 4000원)");
@@ -13,13 +21,26 @@ public class User {
         System.out.println("3. 카푸치노 (가격 : 6000원)");
         System.out.print("> ");
         int buySelect = Integer.parseInt(scanner.nextLine());
+        System.out.println("커피를 구매하였습니다. 감사합니다.");
         if(buySelect == 1){
             ingredient.setMoney(ingredient.getMoney() + espressoMoney);
+            ingredient.setWater(ingredient.getWater() - espressoWater);
+            ingredient.setCoffeeBeans(ingredient.getCoffeeBeans() - espressoCoffeeBeans);
+            System.out.println("에스프레소가 준비되었습니다. 맛있게 드세요.");
         } else if(buySelect == 2){
             ingredient.setMoney(ingredient.getMoney() + latteMoney);
+            ingredient.setWater(ingredient.getWater() - latteWater);
+            ingredient.setMilk(ingredient.getMilk() - latteMilk);
+            ingredient.setCoffeeBeans(ingredient.getCoffeeBeans() - latteCoffeeBeans);
+            System.out.println("라떼가 준비되었습니다. 맛있게 드세요.");
         } else if(buySelect == 3) {
             ingredient.setMoney(ingredient.getMoney() + cappuccinoMoney);
+            ingredient.setWater(ingredient.getWater() - cappuccinoWater);
+            ingredient.setMilk(ingredient.getMilk() - cappuccinoMilk);
+            ingredient.setCoffeeBeans(ingredient.getCoffeeBeans() - cappuccinoCoffeeBeans);
+            System.out.println("카푸치노가 준비되었습니다. 맛있게 드세요.");
         }
+        ingredient.setDisposableCup(ingredient.getDisposableCup() - 1);
         coffeeMachineState();
     }
 
